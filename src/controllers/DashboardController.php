90394ed72325ac849358e3b3b36e08f3ce74fb1a
michalsiudut <michal.siudut.ms@gmail.com>
2025-11-24 14:06:51 +0100

    added connection to database

--- src/controllers/DashboardController.php
+++ src/controllers/DashboardController.php
@@ -1,6 +1,7 @@
 <?php
 
 require_once  'AppController.php';
+require_once __DIR__.'/../repository/UserRepository.php';
 
 class DashboardController extends AppController{
 
@@ -54,6 +55,9 @@ class DashboardController extends AppController{
                 ],
             ];
         }
+        $userRepository = new UserRepository();
+        $users = $userRepository->getUsers();
+        var_dump($users);
 
         return $this->render("dashboard", [
             "cards" => $cards
